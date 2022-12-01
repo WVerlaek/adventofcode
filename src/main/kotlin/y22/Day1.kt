d47349cb94515debe433bb27dddf1ad22463df12
Wouter Verlaek <wouter@gitpod.io>
2022-12-01 09:04:37 +0000

    Add 2022 day 1

---
+++ src/main/kotlin/y22/Day1.kt
@@ -0,0 +1,17 @@
+package y22
+
+import common.puzzle.Input
+import common.puzzle.Puzzle
+import common.puzzle.solvePuzzle
+
+fun main() = solvePuzzle(year = 2022, day = 1, level = 1) { Day1(it) }
+
+class Day1(val input: Input) : Puzzle {
+    override fun solveLevel1(): Any {
+        TODO()
+    }
+
+    override fun solveLevel2(): Any {
+        TODO()
+    }
+}
