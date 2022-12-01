e55b08b5e053691b7340d516722ec09b4f772b13
Wouter Verlaek <wouter@gitpod.io>
2022-12-01 10:33:48 +0000

    Install kotlin and vs code extensions

---
+++ .gitpod.Dockerfile
@@ -0,0 +1,3 @@
+FROM gitpod/workspace-full:latest
+
+RUN sdk install kotlin
--- src/main/kotlin/y22/Day1.kt
+++ src/main/kotlin/y22/Day1.kt
@@ -8,7 +8,19 @@ fun main() = solvePuzzle(year = 2022, day = 1, level = 1) { Day1(it) }
 
 class Day1(val input: Input) : Puzzle {
     override fun solveLevel1(): Any {
-        TODO()
+        var max = 0
+        var cur = 0
+        input.lines.forEach { line ->
+            if (line.isEmpty()) {
+                if (cur > max) {
+                    max = cur
+                }
+                cur = 0
+            } else {
+                cur += line.toInt()
+            }
+        }
+        return max
     }
 
     override fun solveLevel2(): Any {
