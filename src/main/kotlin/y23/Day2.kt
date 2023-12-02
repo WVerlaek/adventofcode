73655c6b050d3c4e14ed6d5b27f1a04b3aa430ef
Wouter Verlaek <wverlaek@hotmail.nl>
2023-12-02 12:15:56 +0100

    Day 2 part 1

--- src/main/kotlin/y23/Day2.kt
+++ src/main/kotlin/y23/Day2.kt
@@ -15,8 +15,52 @@ fun main() = solvePuzzle(year = 2023, day = 2) { Day2(it) }
 
 class Day2(val input: Input) : Puzzle {
 
+    data class Game(
+        val id: Int,
+        val sets: List<CubeSet>,
+    ) {
+        fun isPossible(cubes: Map<String, Int>): Boolean {
+            return sets.all { it.isPossible(cubes) }
+        }
+    }
+
+    data class CubeSet(
+        val cubes: Map<String, Int>,
+    ) {
+        fun isPossible(cubes: Map<String, Int>): Boolean {
+            return this.cubes.all { (col, count) ->
+                (cubes[col] ?: 0) >= count
+            }
+        }
+    }
+
+    private fun parseGame(line: String): Game {
+        val id = line.substring("Game ".length, line.indexOf(':'))
+            .toInt()
+
+        val sets = line.substringAfter(": ")
+            .split("; ")
+            .map { set ->
+                val cubes = set.split(", ")
+                    .associate { cube ->
+                        val (count, col) = cube.split(" ")
+                        col to count.toInt()
+                    }
+                CubeSet(cubes)
+            }
+
+        return Game(id, sets)
+    }
+
     override fun solveLevel1(): Any {
-        TODO()
+        val games = input.lines.map(::parseGame)
+        val bag = mapOf(
+            "red" to 12,
+            "green" to 13,
+            "blue" to 14,
+        )
+        return games.filter { game -> game.isPossible(bag) }
+            .sumOf { it.id }
     }
 
     override fun solveLevel2(): Any {
--- src/test/kotlin/y23/Day2Test.kt
+++ src/test/kotlin/y23/Day2Test.kt
@@ -6,14 +6,18 @@ import org.junit.jupiter.api.Test
 
 internal class Day2Test {
     private val sample = Input("""
-
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     """.trimIndent())
 
     private val day = Day2(sample)
 
     @Test
     fun solveLevel1() {
-        assertEquals(, day.solveLevel1())
+        assertEquals(8, day.solveLevel1())
     }
 
     @Test
